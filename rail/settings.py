fa39383bed8a2c8c4a39006a6692457fb714b845
nmogulla-uno <89790653+nmogulla@users.noreply.github.com>
2022-03-09 16:25:32 -0600

    Update settings.py

--- rail/settings.py
+++ rail/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
